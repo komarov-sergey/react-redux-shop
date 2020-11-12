fc8404759996e48c9b535c360490d19b9b3694a2
Sergey <komarovs33@mail.ru>
2020-11-12 18:18:30 +0300

    qf

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -18,12 +18,12 @@ const Search = () => {
       <div className="input-group">
         <form onSubmit={handleSubmit}>
           <input onChange={handleChange} type="text" className="form-control" />
-          <span className="input-group-btn">
-            <button className="btn btn-default">
-              <span className="glyphicon glyphicon-search"></span>
-            </button>
-          </span>
         </form>
+        <span className="input-group-btn">
+          <button className="btn btn-default">
+            <span className="glyphicon glyphicon-search"></span>
+          </button>
+        </span>
       </div>
     </div>
   )
